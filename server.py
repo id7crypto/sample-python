bde222d83d4f256f47ce2041ddc1b02ccbf2963a
id7crypto <97322327+id7crypto@users.noreply.github.com>
2022-02-26 15:42:01 +0100

    Show the AD table on refresh

--- server.py
+++ server.py
@@ -7,10 +7,27 @@ from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+        try:
+    
+            filepath = os.path.join("/home/ADBot", "table.html")
+            if not os.path.exists:
+                print('table.html not found')
+    
+            f = open(filepath, 'rb')
+    
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            self.wfile.write(f.read().strip())
+            f.close()
+        except Exception as e:
+            print(e)
+            self.send_response(400)
+#    def do_GET(self):
+#        self.send_response(HTTPStatus.OK)
+#        self.end_headers()
+#        dirs = os.listdir("/home/")
+#        msg = 'Hello! you requested %s' % (self.path)
+#        self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
