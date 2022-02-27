cddeec9ade3d2125c7efaf675bcdbef18a54c671
id7crypto <97322327+id7crypto@users.noreply.github.com>
2022-02-27 12:09:22 +0100

    Show also update time

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import datetime as dt
 import http.server
 import socketserver
 
@@ -12,25 +13,23 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             filepath = os.path.join("/home/ADBot", "table.html")
             if not os.path.exists:
                 print('table.html not found')
+                self.send_response(400, message='table.html not found')
+                return
     
             f = open(filepath, 'rb')
+            t = dt.datetime.fromtimestamp(os.path.getmtime(filepath), tz=dt.timezone(dt.timedelta(hours=1)))
     
             self.send_response(HTTPStatus.OK)
             self.end_headers()
+            self.wfile.write(f"<html>{t.strftime('%Y-%m-%d %H:%M:%S %Z')}</html>\n".encode())
             self.wfile.write(f.read().strip())
             f.close()
         except Exception as e:
             print(e)
             self.send_response(400)
-#    def do_GET(self):
-#        self.send_response(HTTPStatus.OK)
-#        self.end_headers()
-#        dirs = os.listdir("/home/")
-#        msg = 'Hello! you requested %s' % (self.path)
-#        self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
+print('Listening on port %s' % port)
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
